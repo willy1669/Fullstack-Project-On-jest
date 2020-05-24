bc40ddf4dceca65e5f54a191d9f61173f8eaedbf
chinonso Wiliiams <williamschinonso11gmail.com>
2020-05-24 15:15:59 +0100

    create mock for mails

---
+++ auth-app/__mocks__/@fullstackjs/mail.js
@@ -0,0 +1,25 @@
+module.exports = class Mail {
+
+    to() {
+
+        return this
+    }
+
+    subject() {
+
+        return this
+
+    }
+
+    data() {
+
+        return this
+
+    }
+
+    send () {
+
+        return this
+    }
+
+}
